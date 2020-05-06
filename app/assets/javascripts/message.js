74b0cc8fe9ca91f842848659318e48665f82f742
nittaikuya <64181601+nittaikuya@users.noreply.github.com>
2020-05-06 16:33:35 +0900

    jidokoushin

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
       if ( message.image ) {
         var html =
-          `<div class="message">
+          `<div class="message" data-message-id=${message.id}>
             <div class="chat-main__chat">
               <div class="chat-main__chat__upper-info">
                 <div class="chat-main__chat__upper-info__talker">
@@ -21,7 +21,7 @@ $(function(){
         return html;
       } else {
         var html =
-          `<div class="message">
+          `<div class="message" data-message-id=${message.id}>
             <div class="chat-main__chat">
               <div class="chat-main__chat__upper-info">
                 <div class="chat-main__chat__upper-info__talker">
@@ -65,6 +65,32 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
   });
 })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
 
 
