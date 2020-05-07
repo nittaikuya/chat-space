0261ff8b0a043b1c993e8f8e20fbaccd3d3a9cfa
nittaikuya <64181601+nittaikuya@users.noreply.github.com>
2020-05-07 17:40:34 +0900

    Capfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,7 @@
+server '18.180.94.177', user: 'ec2-user', roles: %w{app db web}
+
+
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
