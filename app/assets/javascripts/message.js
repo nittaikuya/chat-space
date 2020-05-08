6929775d1af3c9e2772c3b61fd78c6395077377d
nittaikuya <64181601+nittaikuya@users.noreply.github.com>
2020-05-08 19:46:46 +0900

    shusei2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -72,6 +72,7 @@ $('#new_message').on('submit', function(e){
 
   var reloadMessages = function() {
     var last_message_id = $('.message:last').data("message-id");
+    console.log(last_message_id);
     $.ajax({
       url: "api/messages",
       type: 'get',
@@ -79,13 +80,14 @@ $('#new_message').on('submit', function(e){
       data: {id: last_message_id}
     })
     .done(function(messages) {
+      console.log(messages)
       if (messages.length !== 0) {
         var insertHTML = '';
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
         $('.messages').append(insertHTML);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('.chat-main__chats').animate({ scrollTop: $('.chat-main__chats')[0].scrollHeight});
       }
     })
     .fail(function() {
